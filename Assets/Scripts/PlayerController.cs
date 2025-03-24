dc4a3430b246d090b88c882c06587375e672d5c7
4Rmz999 <patipong.saet@bumail.net>
2025-03-24 23:02:20 +0700

    add text add fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    public Slider chargeSlider;
+
     public float maxChargeTime = 3f;
-    public float maxForce = 20f;
+    public float maxForce = 30f;
     public Vector3 jumpDirection = new Vector3(0, 1, 1);
     public int maxJumps = 3;
 
@@ -13,7 +16,7 @@ public class PlayerController : MonoBehaviour
     private bool isCharging = false;
     private Rigidbody rb;
     private bool hasJumped = false;
-    private int jumpCount = 0;
+    
 
     public float minY = -10f;   
     public float maxY = 50f;
@@ -22,15 +25,24 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+
+        if (chargeSlider != null)
+            chargeSlider.maxValue = maxChargeTime;
+
     }
 
     void Update()
     {
+
         if (transform.position.y < minY || transform.position.y > maxY)
         {
             
             ReloadScene();
+        }
+
+        if (isCharging && chargeSlider != null)
+        {
+            chargeSlider.value = chargeTimer;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && !hasJumped)
@@ -48,6 +60,8 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Space) && isCharging)
         {
             Gravity();
+            if (chargeSlider != null)
+                chargeSlider.value = 0;
         }
     }
 
@@ -83,10 +97,10 @@ public class PlayerController : MonoBehaviour
 
         isCharging = false;
         hasJumped = true;
-        jumpCount++;
+        
 
         Debug.Log(appliedForce);
-        Debug.Log(jumpCount);
+        
     }
 
 
