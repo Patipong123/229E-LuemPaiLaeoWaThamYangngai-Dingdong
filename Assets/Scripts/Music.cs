b00a19dbc4b7ae8783ccc31fb56448feed4d0908
Patipong123 <patipong.saet@bumail.net>
2025-03-29 02:13:49 +0700

    finish project and build

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public static Music instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
